6cfdea71797fff4737fcc9b469947bdaefdedf5c
lu4ezar <misha-shisha@mail.ru>
2020-04-13 19:40:03 +0700

    Updates

---
+++ precache-manifest.ec7763c47ad8fc4aa700173acf557fa8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffd78fb069bb35f28e3110b43feb774e",
+    "url": "/e-shop/index.html"
+  },
+  {
+    "revision": "1766c6ee713d4cb3546f",
+    "url": "/e-shop/static/css/main.7c22e596.chunk.css"
+  },
+  {
+    "revision": "402d697956913ca51efe",
+    "url": "/e-shop/static/js/2.cfd88b34.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/e-shop/static/js/2.cfd88b34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1766c6ee713d4cb3546f",
+    "url": "/e-shop/static/js/main.15d4a204.chunk.js"
+  },
+  {
+    "revision": "37f3844c37cea9b1fba6",
+    "url": "/e-shop/static/js/runtime-main.eb5ec247.js"
+  }
+]);

5a416268e0aab518d510c5011ec5888dae0f54e2
lu4ezar <misha-shisha@mail.ru>
2020-04-14 01:38:54 +0700

    + Cart type

--- src/redux/cartSlice.ts
+++ src/redux/cartSlice.ts
@@ -1,8 +1,14 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+export type Cart = Array<{
+  title: string;
+  price: string;
+  quantity: number;
+}>;
+
 export const cartSlice = createSlice({
   name: "cart",
-  initialState: {} as any,
+  initialState: {} as Cart,
   reducers: {
     addProduct: (state, action) => action.payload,
     removeProduct: (state, action) => action.payload,

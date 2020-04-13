f652eb9895bab53272f6dff02bdee672ddcb1e62
lu4ezar <misha-shisha@mail.ru>
2020-04-13 23:03:08 +0700

    + ProductCard

--- src/components/Homepage/Homepage.tsx
+++ src/components/Homepage/Homepage.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import HeadingText from "../shared/HeadingText";
+import ProductCard from "../ProductCard";
+import "./Homepage.scss";
 
 const Home = () => (
   <div>
     <HeadingText pxSize={32}>Homepage</HeadingText>
+    <HeadingText pxSize={16}>Products we sell:</HeadingText>
+    <div className="product_gallery">
+      <ProductCard productName="Lady Bg Pillow">PRODUCT 1</ProductCard>
+      <ProductCard productName="Lady Gaga Pillow">PRODUCT 2</ProductCard>
+      <ProductCard productName="Gucci Gang Pillow">PRODUCT 3</ProductCard>
+    </div>
   </div>
 );
 
---
+++ src/components/ProductCard/ProductCard.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./ProductCard.scss";
+import HeadingText from "../shared/HeadingText";
+
+const ProductCard = (props: any) => (
+  <div className="ProductCard">
+    <HeadingText pxSize={16}>{props.productName}</HeadingText>
+    <img alt="product" src="https://www.fillmurray.com/250/300" />
+    <span>Super Price: $99</span>
+    <button>Add to cart</button>
+  </div>
+);
+
+export default ProductCard;

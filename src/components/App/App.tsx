98514d4517d201d4d8826f8fafab833978ce76f7
lu4ezar <misha-shisha@mail.ru>
2020-04-13 19:18:38 +0700

    added Router navigation

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,15 +1,27 @@
 import React from "react";
+import { HashRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Homepage from "../Homepage";
 import Cart from "../Cart";
 import Order from "../Order";
 
 function App() {
   return (
-    <div>
-      <Homepage />
-      <Cart />
-      <Order />
-    </div>
+    <Router>
+      <Link to="/">Home</Link>
+      <Link to="/cart">Cart</Link>
+      <Link to="/order">Order</Link>
+      <Switch>
+        <Route path="/cart">
+          <Cart />
+        </Route>
+        <Route path="/order">
+          <Order />
+        </Route>
+        <Route path="/">
+          <Homepage />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 

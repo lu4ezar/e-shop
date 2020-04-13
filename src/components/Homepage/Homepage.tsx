656e1881dbc2169d55125897bf0f7763993154f0
lu4ezar <misha-shisha@mail.ru>
2020-04-13 23:10:30 +0700

    removed placeholder text from product cards

--- src/components/Homepage/Homepage.tsx
+++ src/components/Homepage/Homepage.tsx
@@ -8,9 +8,9 @@ const Home = () => (
     <HeadingText pxSize={32}>Homepage</HeadingText>
     <HeadingText pxSize={16}>Products we sell:</HeadingText>
     <div className="product_gallery">
-      <ProductCard productName="Lady Bg Pillow">PRODUCT 1</ProductCard>
-      <ProductCard productName="Lady Gaga Pillow">PRODUCT 2</ProductCard>
-      <ProductCard productName="Gucci Gang Pillow">PRODUCT 3</ProductCard>
+      <ProductCard productName="Lady Bg Pillow" />
+      <ProductCard productName="Lady Gaga Pillow" />
+      <ProductCard productName="Gucci Gang Pillow" />
     </div>
   </div>
 );

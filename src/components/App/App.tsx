c366019cb0ff48a3aaa342c1a897f74256125783
lu4ezar <misha-shisha@mail.ru>
2020-04-13 18:34:52 +0700

    + Order

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Home from "../Homepage";
 import Cart from "../Cart";
+import Order from "../Order";
 
 function App() {
   return (
     <div>
       <Home />
       <Cart />
+      <Order />
     </div>
   );
 }

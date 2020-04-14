28d379e82628c0d08274b852cfb3d63150fb6040
lu4ezar <misha-shisha@mail.ru>
2020-04-14 10:07:19 +0700

    Cart: + total price

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import HeadingText from "../shared/HeadingText";
+import { useSelector } from "react-redux";
+import { getTotalPrice } from "../../redux/selectors";
 
-const Cart = () => (
-  <div>
-    <HeadingText pxSize={32}>Cart</HeadingText>
-    <div>There are 500 products in your cart.</div>
-    <Link to="/order">BUY</Link>
-  </div>
-);
+const Cart = () => {
+  const totalSum = useSelector(getTotalPrice);
+  return (
+    <div>
+      <HeadingText pxSize={32}>Cart</HeadingText>
+      <div>There are about 500 products in your cart.</div>
+      <div>Total Sum: ${totalSum}</div>
+      <Link to="/order">BUY</Link>
+    </div>
+  );
+};
 
 export default Cart;

f5722a0cee0ed22a444ce0e914370e6f9b3996d9
lu4ezar <misha-shisha@mail.ru>
2020-04-15 01:52:11 +0700

    cartSlice: + removeProduct

--- src/redux/cartSlice.ts
+++ src/redux/cartSlice.ts
@@ -13,9 +13,12 @@ export const cartSlice = createSlice({
         [payload]: curValue + 1,
       };
     },
+    removeProduct: (state, action: PayloadAction<Cart["id"]>) => {
+      const { payload } = action;
+      const curValue = (state[payload as keyof Cart] as number) || 0;
       return {
         ...state,
-        [payload]: prevValue + 1,
+        [payload]: curValue >= 1 ? curValue - 1 : 0,
       };
     },
     deleteProduct: (state, action: PayloadAction<Cart["id"]>) => {

b91617699a255374c542e6a71e851d2acd20ba9b
lu4ezar <misha-shisha@mail.ru>
2020-04-15 00:11:45 +0700

    + CartWidget index + styles

--- src/components/CartWidget/CartWidget.tsx
+++ src/components/CartWidget/CartWidget.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { getTotalProduct } from "../../redux/selectors";
+import "./CartWidget.scss";
 
 const CartWidget = () => {
   const totalProducts = useSelector(getTotalProduct);
   return (
-    <>
+    <div className="CartWidget">
       <div>CartWidget</div>
       <div>{totalProducts}</div>
       <div>Go to cart</div>
-    </>
+    </div>
   );
 };
 export default CartWidget;

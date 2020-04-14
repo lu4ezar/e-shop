8f2663d790a1486cc5f90ec1ffd34f168fb2e65c
lu4ezar <misha-shisha@mail.ru>
2020-04-14 10:18:04 +0700

    Homepage: render product list, add product to cart

--- src/components/Homepage/Homepage.tsx
+++ src/components/Homepage/Homepage.tsx
@@ -3,22 +3,31 @@ import HeadingText from "../shared/HeadingText";
 import ProductCard from "../ProductCard";
 import "./Homepage.scss";
 import { cartSlice } from "../../redux/cartSlice";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { Product } from "../../interfaces";
+import { selectCatalog } from "../../redux/selectors";
 
 const Home = () => {
   const dispatch = useDispatch();
   const { addProduct } = cartSlice.actions;
-  const handleClick = (product: number) => {
-    dispatch(addProduct(1));
+  const catalog = useSelector(selectCatalog);
+  const handleClick = (product: Product) => {
+    dispatch(addProduct(product));
   };
   return (
     <div>
       <HeadingText pxSize={32}>Homepage</HeadingText>
       <HeadingText pxSize={16}>Products we sell:</HeadingText>
       <div className="product_gallery">
-        <ProductCard productName="Lady Bg Pillow" onClick={handleClick} />
-        <ProductCard productName="Lady Gaga Pillow" onClick={handleClick} />
-        <ProductCard productName="Gucci Gang Pillow" onClick={handleClick} />
+        {catalog
+          ? catalog.map(({ isbn13, ...productData }: Product) => (
+              <ProductCard
+                key={isbn13}
+                {...productData}
+                onClick={() => handleClick(productData)}
+              />
+            ))
+          : null}
       </div>
     </div>
   );

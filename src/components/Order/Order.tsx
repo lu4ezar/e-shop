2541d114c835944f7c2cbc6f6b2d710f31a8cd83
lu4ezar <misha-shisha@mail.ru>
2020-04-14 10:18:47 +0700

    Order: show sum

--- src/components/Order/Order.tsx
+++ src/components/Order/Order.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import HeadingText from "../shared/HeadingText";
+import { useSelector } from "react-redux";
+import { getTotalPrice } from "../../redux/selectors";
 
-const Order = () => (
-  <div>
-    <HeadingText pxSize={32}>Order</HeadingText>
-  </div>
-);
+const Order = () => {
+  const sum = useSelector(getTotalPrice);
+  return (
+    <div>
+      <HeadingText pxSize={32}>Order</HeadingText>
+      <div>{sum}</div>
+    </div>
+  );
+};
 
 export default Order;

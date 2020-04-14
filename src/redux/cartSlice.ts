86b0cd0a184ac8efa54d62e63d151b0d1edc0052
lu4ezar <misha-shisha@mail.ru>
2020-04-15 00:24:22 +0700

    cartSlice: data shape + addProduct

--- src/redux/cartSlice.ts
+++ src/redux/cartSlice.ts
@@ -1,12 +1,17 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { Cart } from "../interfaces";
 
 export const cartSlice = createSlice({
   name: "cart",
-  initialState: [] as Cart,
+  initialState: {} as Cart,
   reducers: {
-    addProduct: (state, action) => {
-      state.push(action.payload);
+    addProduct: (state, action: PayloadAction<Cart["id"]>) => {
+      const { payload } = action;
+      const prevValue = (state[payload as keyof Cart] as number) || 0;
+      return {
+        ...state,
+        [payload]: prevValue + 1,
+      };
     },
     removeProduct: (state, action) => {},
   },

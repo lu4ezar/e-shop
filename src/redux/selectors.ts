90c66c613c29f695c6a3b88a9943ff4f0ee8e17a
lu4ezar <misha-shisha@mail.ru>
2020-04-15 11:11:48 +0700

    refactor: selectors

--- src/redux/selectors.ts
+++ src/redux/selectors.ts
@@ -1,6 +1,6 @@
 import { createSelector } from "@reduxjs/toolkit";
 import { RootState } from "./store";
-import { Product, Cart } from "../interfaces";
+import { Product, CartItem } from "../interfaces";
 
 export const selectCart = (state: RootState) => state.cart;
 
@@ -14,44 +14,41 @@ const getCartIds = createSelector(selectCart, (cart) => Object.keys(cart));
 
 const getCartPrices = createSelector(
   [selectCatalog, getCartIds],
-  (catalog, ids) => {
-    return ids.map((id) => {
+  (catalog, ids): CartItem =>
+    ids.reduce((acc, cur) => {
       const { price } = catalog.find(
-        (product: Product) => product.isbn13 === id
-      )!;
-      return { [id]: parseFloat(price.replace(/[$]/g, "")) };
-    });
-  }
+        (product: Product) => product.isbn13 === cur
+      ) as Product;
+      const parsedPrice = parseFloat(price.replace(/[$]/g, ""));
+      return { ...acc, [cur]: parsedPrice };
+    }, {})
 );
 
 export const getCartQuantities = createSelector(
   [selectCart, getCartIds],
-  (cart, ids) => {
-    return ids.map((id) => {
-      return { [id]: cart[id as keyof Cart] as number };
-    });
-  }
+  (cart, ids): CartItem =>
+    ids.reduce((acc, cur) => ({ ...acc, [cur]: cart[cur] }), {})
 );
 
 export const getTotalPrice = createSelector(
   [getCartIds, getCartPrices, getCartQuantities],
   (cartIds, prices, quantities) => {
-    const sum = cartIds.reduce((acc, id) => {
-      const price = prices.find((p) => p[id])![id];
-      let quantity = quantities.find((q) => q[id])?.id;
-      return acc + price * (quantity || 0);
+    const sum = cartIds.reduce((acc, cur) => {
+      const price = prices[cur];
+      const quantity = quantities[cur];
+      return acc + price * quantity;
     }, 0);
     return Math.round((sum + Number.EPSILON) * 100) / 100;
   }
 );
 
-export const getProductsByIds = createSelector(
+export const getCartProductsInfo = createSelector(
   [getCartIds, getCartQuantities, selectCatalog],
   (cartIds, quantities, catalog) => {
     return cartIds.map((id) => {
       const product = catalog.find((product) => product.isbn13 === id)!;
       const { title, price } = product;
-      const quantity = quantities.find((q) => q[id])![id];
+      const quantity = quantities[id];
       return { id, title, price, quantity };
     });
   }

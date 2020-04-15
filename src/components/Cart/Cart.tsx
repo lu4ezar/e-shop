ed795a326c3deebe75a7065beb332350ece8ae7c
lu4ezar <misha-shisha@mail.ru>
2020-04-15 11:11:27 +0700

    added decrement and delete to Cart

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,19 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import HeadingText from "../shared/HeadingText";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { cartSlice } from "../../redux/cartSlice";
 import {
   getTotalProduct,
   getTotalPrice,
-  getProductsByIds,
+  getCartProductsInfo,
 } from "../../redux/selectors";
-import { CartInfo } from "../../interfaces";
+import { CartInfo, Id } from "../../interfaces";
 import "./Cart.scss";
 
 const Cart = () => {
   const totalProduct = useSelector(getTotalProduct);
   const totalPrice = useSelector(getTotalPrice);
-  const cartInfo = useSelector(getProductsByIds);
+  const cartInfo = useSelector(getCartProductsInfo);
+  const { removeProduct, deleteProduct } = cartSlice.actions;
+  const dispatch = useDispatch();
+  const handleDelete = (id: Id) => {
+    dispatch(deleteProduct(id));
+  };
+  const handleDecrement = (id: Id) => {
+    dispatch(removeProduct(id));
+  };
   return (
     <div className="Cart">
       <HeadingText pxSize={32}>Cart</HeadingText>
@@ -23,6 +32,8 @@ const Cart = () => {
             {title}
             <span>{price}</span>
             <span>Qty: {quantity}</span>
+            <button onClick={() => handleDecrement(id)}> -1 </button>
+            <button onClick={() => handleDelete(id)}>DELETE</button>
           </div>
         ))}
       </div>

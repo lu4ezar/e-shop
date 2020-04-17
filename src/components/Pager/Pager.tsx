e54ad7e9c701f43f42b575cb66ac67060c883739
lu4ezar <misha-shisha@mail.ru>
2020-04-17 19:33:31 +0700

    Pager: disabled buttons

--- src/components/Pager/Pager.tsx
+++ src/components/Pager/Pager.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Pager.scss";
-import { selectPage } from "../../redux/selectors";
+import { selectPage, getTotalPages } from "../../redux/selectors";
 import { useSelector, useDispatch } from "react-redux";
 import { ReactComponent as LeftArrow } from "../../img/left_arrow.svg";
 import { ReactComponent as RightArrow } from "../../img/right_arrow.svg";
@@ -8,15 +8,24 @@ import { pageSlice } from "../../redux/pageSlice";
 
 const Pager = () => {
   const page = useSelector(selectPage);
+  const incDisabled = useSelector(getTotalPages) === page;
   const dispatch = useDispatch();
   const { incrementPage, decrementPage } = pageSlice.actions;
   return (
     <div className="Pager">
-      <button className="arrow_btn" onClick={() => dispatch(decrementPage())}>
+      <button
+        className="arrow_btn"
+        onClick={() => dispatch(decrementPage())}
+        disabled={page === 1}
+      >
         <LeftArrow />
       </button>
       <div className="page_number">{page}</div>
-      <button className="arrow_btn" onClick={() => dispatch(incrementPage())}>
+      <button
+        className="arrow_btn"
+        onClick={() => dispatch(incrementPage())}
+        disabled={incDisabled}
+      >
         <RightArrow />
       </button>
     </div>

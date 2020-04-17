e5835a1513b11398ad487e3ab51a30ce2cbe8217
lu4ezar <misha-shisha@mail.ru>
2020-04-17 19:33:57 +0700

    updated ProductCard

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -3,18 +3,11 @@ import "./ProductCard.scss";
 import HeadingText from "../shared/HeadingText";
 import { ProductCardProps } from "../../interfaces";
 
-const ProductCard = ({
-  title,
-  subtitle,
-  image,
-  price,
-  onClick,
-}: ProductCardProps) => (
+const ProductCard = ({ name, image, price, onClick }: ProductCardProps) => (
   <div className="ProductCard">
-    <HeadingText pxSize={16}>{title}</HeadingText>
-    <div>{subtitle}</div>
+    <HeadingText pxSize={16}>{name}</HeadingText>
     <img alt="product" src={image} />
-    <span>Price: {price}</span>
+    <span>${price}</span>
     <button onClick={onClick}>Add to cart</button>
   </div>
 );

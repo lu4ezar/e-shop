07a759ba37317a1f20739adbc57f6dadf3cef7a7
lu4ezar <misha-shisha@mail.ru>
2020-04-18 11:34:14 +0700

    updated Homepage

--- src/components/Homepage/Homepage.tsx
+++ src/components/Homepage/Homepage.tsx
@@ -1,33 +1,30 @@
 import React from "react";
-import HeadingText from "../shared/HeadingText";
 import ProductCard from "../ProductCard";
 import "./Homepage.scss";
 import { cartSlice } from "../../redux/cartSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { Product, Id } from "../../interfaces";
-import { selectCatalog } from "../../redux/selectors";
+import { getProducts } from "../../redux/selectors";
 import Catalog from "../Catalog";
 import Pager from "../Pager";
 
 const Homepage = () => {
   const dispatch = useDispatch();
   const { addProduct } = cartSlice.actions;
-  const catalog = useSelector(selectCatalog);
+  const catalog = useSelector(getProducts);
   const handleClick = (id: Id) => {
     dispatch(addProduct(id));
   };
   return (
     <div className="Homepage">
-      <HeadingText pxSize={32}>Homepage</HeadingText>
-      <HeadingText pxSize={16}>Products we sell:</HeadingText>
       <Catalog />
       <div className="product_gallery">
         {catalog
-          ? catalog.map(({ isbn13, ...productData }: Product) => (
+          ? catalog.map((product: Product) => (
               <ProductCard
-                key={isbn13}
-                {...productData}
-                onClick={() => handleClick(isbn13 as Id)}
+                key={product.id.toString()}
+                {...product}
+                onClick={() => handleClick(product.id)}
               />
             ))
           : null}

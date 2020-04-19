cadc519a656dadbf591c4c9e04466f2364e59788
lu4ezar <misha-shisha@mail.ru>
2020-04-19 21:07:20 +0700

    added basename prop to router

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ import { Provider } from "react-redux";
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <App />
       </Router>
     </Provider>
